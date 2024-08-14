8f52e5e9840592d1f1ccb9fc0957f84998440ddc
Jelly <jongyun.kim@cranfield.ac.uk>
2024-08-14 12:10:04 +0100

    Data uploaded, collect/view updated

--- experiments/240814/collect_vicsek_rl.py
+++ experiments/240814/collect_vicsek_rl.py
@@ -40,7 +40,7 @@ env = LazyVicsekEnv(config_to_env_input(my_config))
 obs = env.reset()
 num_agents = env.num_agents
 
-num_seeds = 2
+num_seeds = 200
 
 episode_reward_rl = np.zeros(num_seeds)
 episode_length_rl = np.zeros(num_seeds)
@@ -103,3 +103,129 @@ np.savez('../../data/240814/240814_Vicsek.npz',
          episode_length_vicsek=episode_length_vicsek,  # (num_seeds,)
          alignment_hists_vicsek=alignment_hist_vicsek,  # (num_seeds * max_time_steps)
          info_usage_hists_vicsek=info_usage_hist_vicsek)  # (num_seeds * max_time_steps)
+
+
+# import numpy as np
+# import ray
+# from env.envs import LazyVicsekEnv, load_config, config_to_env_input
+# from model.model_rllib import LazyVicsekModelPPO
+# from ray.rllib.policy.policy import Policy
+# from ray.tune.registry import register_env
+# from ray.rllib.models import ModelCatalog
+#
+# # Initialize Ray
+# ray.init(num_cpus=14, num_gpus=0, ignore_reinit_error=True)
+#
+# # Model settings
+# model_name = "vicsek_lazy_listener"
+# ModelCatalog.register_custom_model(model_name, LazyVicsekModelPPO)
+#
+# # Policy settings
+# base_path = "C:/Users/Jelly/ray_results/debugging0812"
+# trial_path = base_path + "/PPO_vicsek_lazy_env_75b51_00000_0_2024-08-12_21-42-59"
+# checkpoint_path = trial_path + "/checkpoint_000029/policies/default_policy"
+# policy = Policy.from_checkpoint(checkpoint_path)
+# policy.model.to("cpu")
+# policy.model.eval()
+#
+# default_config_path = "D:/pych_ws/lazy-vicsek/env_config_train.yaml"
+# my_config = load_config(default_config_path)
+#
+# my_config.env.max_time_steps = 500
+# my_config.env.alignment_window_length = 32
+# my_config.env.alignment_goal = 0.97
+# my_config.env.alignment_rate_goal = 0.02
+# my_config.env.comm_range = 10
+# my_config.control.initial_position_bound = 100.0
+# my_config.control.speed = 5.0
+# my_config.env.num_agents_pool = [20]
+# # my_config.control.max_turn_rate = 8/15
+#
+# my_config.env.use_fixed_episode_length = True
+# my_config.env.ignore_comm_lost_agents = True
+# my_config.env.periodic_boundary = True
+#
+# env = LazyVicsekEnv(config_to_env_input(my_config))
+# num_agents = env.num_agents
+#
+# num_seeds = 14
+#
+# # Allocate arrays
+# episode_reward_rl = np.zeros(num_seeds)
+# episode_length_rl = np.zeros(num_seeds)
+# episode_success_mask_rl = np.zeros(num_seeds, dtype=np.bool_)
+# info_usage_hist_rl = np.zeros((num_seeds, my_config.env.max_time_steps), dtype=np.float32)
+# alignment_hist_rl = np.zeros((num_seeds, my_config.env.max_time_steps), dtype=np.float32)
+#
+# episode_reward_vicsek = np.zeros(num_seeds)
+# episode_length_vicsek = np.zeros(num_seeds)
+# episode_success_mask_vicsek = np.zeros(num_seeds, dtype=np.bool_)
+# info_usage_hist_vicsek = np.ones((num_seeds, my_config.env.max_time_steps), dtype=np.float32)
+# alignment_hist_vicsek = np.zeros((num_seeds, my_config.env.max_time_steps), dtype=np.float32)
+#
+#
+# @ray.remote
+# def run_episode(seed, policy, my_config, num_agents, is_vicsek=False):
+#     env = LazyVicsekEnv(config_to_env_input(my_config))
+#     env.seed(seed)
+#     obs = env.reset()
+#     reward_sum = 0
+#     step = 0
+#     done = False
+#     info_usage_hist = np.zeros(my_config.env.max_time_steps, dtype=np.float32)
+#     alignment_hist = np.zeros(my_config.env.max_time_steps, dtype=np.float32)
+#
+#     while not done:
+#         step += 1
+#         if is_vicsek:
+#             action = env.get_vicsek_action()
+#         else:
+#             action = env.get_vicsek_action()
+#             action2, _, action2_info = policy.compute_single_action(obs, explore=True)
+#             info_usage = (action2.sum() - num_agents) / (action.sum() - num_agents)
+#             info_usage_hist[step - 1] = info_usage
+#             action = action2
+#
+#         obs, reward, done, info = env.step(action)
+#         reward_sum += reward
+#         alignment_hist[step - 1] = env.alignment_hist[step - 1]
+#
+#     return reward_sum, env.time_step, env.time_step < env.config.env.max_time_steps, info_usage_hist, alignment_hist
+#
+#
+# # Execute in parallel using Ray
+# rl_results = ray.get(
+#     [run_episode.remote(seed, policy, my_config, num_agents, is_vicsek=False) for seed in range(num_seeds)])
+# vicsek_results = ray.get(
+#     [run_episode.remote(seed, policy, my_config, num_agents, is_vicsek=True) for seed in range(num_seeds)])
+#
+# # Aggregate the results
+# for i, (reward_sum, time_step, success_mask, info_usage_hist, alignment_hist) in enumerate(rl_results):
+#     episode_reward_rl[i] = reward_sum
+#     episode_length_rl[i] = time_step
+#     episode_success_mask_rl[i] = success_mask
+#     info_usage_hist_rl[i, :time_step] = info_usage_hist[:time_step]
+#     alignment_hist_rl[i, :time_step] = alignment_hist[:time_step]
+#
+# for i, (reward_sum, time_step, success_mask, info_usage_hist, alignment_hist) in enumerate(vicsek_results):
+#     episode_reward_vicsek[i] = reward_sum
+#     episode_length_vicsek[i] = time_step
+#     episode_success_mask_vicsek[i] = success_mask
+#     info_usage_hist_vicsek[i, :time_step] = info_usage_hist[:time_step]
+#     alignment_hist_vicsek[i, :time_step] = alignment_hist[:time_step]
+#
+# # Save results
+# np.savez('../../data/240814/240814_RL.npz',
+#          episode_reward_rl=episode_reward_rl,
+#          episode_length_rl=episode_length_rl,
+#          alignment_hists_rl=alignment_hist_rl,
+#          info_usage_hists_rl=info_usage_hist_rl)
+#
+# np.savez('../../data/240814/240814_Vicsek.npz',
+#          episode_reward_vicsek=episode_reward_vicsek,
+#          episode_length_vicsek=episode_length_vicsek,
+#          alignment_hists_vicsek=alignment_hist_vicsek,
+#          info_usage_hists_vicsek=info_usage_hist_vicsek)
+#
+# # Shutdown Ray
+# ray.shutdown()
--- experiments/240814/view_vicsek_rl.py
+++ experiments/240814/view_vicsek_rl.py
@@ -5,24 +5,36 @@ import matplotlib.pyplot as plt
 data_rl = np.load('../../data/240814/240814_RL.npz')
 data_vicsek = np.load('../../data/240814/240814_Vicsek.npz')
 
-# Extract the required data
-episode_reward_rl = data_rl['episode_reward_RL']  # (40,)
-last_alignments_rl = data_rl['last_alignments_RL']  # (40,)
-info_usage_rl = data_rl['info_usage_hist_RL']  # (40, 500)
-alignment_hist_40_rl = data_rl['alignment_hist_seed_40_RL']  # (500,)
-#
-episode_reward_vicsek = data_vicsek['episode_reward_vicsek']
-last_alignments_vicsek = data_vicsek['last_alignments_vicsek']
-info_usage_vicsek = data_vicsek['info_usage_hist_vicsek']
-alignment_hist_40_vicsek = data_vicsek['alignment_hist_seed_40_vicsek']
+episodic_reward_rl = data_rl['episode_reward_rl']    # (num_seeds,)
+order_parameter_rl = data_rl['alignment_hists_rl']   # (num_seeds, max_time_steps) dt = 0.1s
+info_util_ratio_rl = data_rl['info_usage_hists_rl']  # (num_seeds, max_time_steps)
+
+episodic_reward_vicsek = data_vicsek['episode_reward_vicsek']    # (num_seeds,)
+order_parameter_vicsek = data_vicsek['alignment_hists_vicsek']   # (num_seeds, max_time_steps) dt = 0.1s
+info_util_ratio_vicsek = data_vicsek['info_usage_hists_vicsek']  # (num_seeds, max_time_steps)
+
+num_seeds = episodic_reward_rl.shape[0]
+max_time_steps = order_parameter_rl.shape[1]
 
 # Calculate averages
-avg_episode_reward_rl = np.mean(episode_reward_rl)
-avg_episode_reward_vicsek = np.mean(episode_reward_vicsek)
-avg_last_alignment_rl = np.mean(last_alignments_rl)
-avg_last_alignment_vicsek = np.mean(last_alignments_vicsek)
-avg_info_usage_rl = np.mean(info_usage_rl)
-avg_info_usage_vicsek = np.mean(info_usage_vicsek)
+avg_episode_reward_rl = np.mean(episodic_reward_rl)
+avg_episode_reward_vicsek = np.mean(episodic_reward_vicsek)
+avg_last_alignment_rl = np.mean(order_parameter_rl[:, -1])
+avg_last_alignment_vicsek = np.mean(order_parameter_vicsek[:, -1])
+avg_info_usage_rl = np.mean(info_util_ratio_rl)
+avg_info_usage_vicsek = np.mean(info_util_ratio_vicsek)
+
+# Define their colors
+vs_color = 'tab:blue'  # Vicsek color
+rl_color = 'tab:green'
+
+# Define grid style
+plt.rcParams.update({
+    'grid.color': 'gray',        # Set grid color to gray
+    'grid.linestyle': '--',      # Set grid linestyle to dashed
+    'grid.linewidth': 0.6,       # Set grid linewidth
+    'grid.alpha': 0.7            # Set grid transparency (alpha)
+})
 
 # Define a common width for the bars with hatch for Vicsek bars and no pattern for RL
 bar_width = 0.4
@@ -30,48 +42,71 @@ bar_width = 0.4
 # Update font size and weight
 plt.rcParams.update({'font.size': 16, 'font.weight': 'bold'})
 
-# 1. Average episode reward with hatch pattern for Vicsek and no pattern for RL
+# 1. Average episode reward
 plt.figure(figsize=(8, 6))
-plt.bar(['Vicsek', 'RL'], [avg_episode_reward_vicsek, avg_episode_reward_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''])
-plt.title('Average Episode Reward')
+plt.bar(['Vicsek', 'RL'], [avg_episode_reward_vicsek, avg_episode_reward_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''], zorder=3)
+plt.title(f'Average Episode Reward ({num_seeds} experiments)')
 plt.ylabel('Average Reward')
+plt.grid(axis='y', zorder=0)
+plt.savefig('../../data/240814/figs/240814_avg_episode_reward.png')
 plt.show()
 
-# 2. Replace "last alignment" with "order parameter at t=50s"
+# 2. Average Order Parameter at t=50s
 plt.figure(figsize=(8, 6))
-plt.bar(['Vicsek', 'RL'], [avg_last_alignment_vicsek, avg_last_alignment_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''])
-plt.title('Average Order Parameter at t=50s')
+plt.bar(['Vicsek', 'RL'], [avg_last_alignment_vicsek, avg_last_alignment_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''], zorder=3)
+plt.title(f'Average Order Parameter at t=50s ({num_seeds} experiments)')
 plt.ylabel('Average Order Parameter')
+plt.grid(axis='y', zorder=0)
+plt.savefig('../../data/240814/figs/240814_avg_order_parameter.png')
 plt.show()
 
-# 3. Average information usage with hatch pattern for Vicsek and no pattern for RL
+# 3. Average Information Utilization Ratio
 plt.figure(figsize=(8, 6))
-plt.bar(['Vicsek', 'RL'], [avg_info_usage_vicsek, avg_info_usage_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''])
-plt.title('Average Information Usage')
-plt.ylabel('Average Usage')
+plt.bar(['Vicsek', 'RL'], [avg_info_usage_vicsek, avg_info_usage_rl], color=['tab:blue', 'tab:green'], width=bar_width, hatch=['/', ''], zorder=3)
+plt.title(f'Average Information Utilization Ratio ({num_seeds} experiments)')
+plt.ylabel('Average Utilization Ratio')
+plt.grid(axis='y', zorder=0)
+plt.savefig('../../data/240814/figs/240814_avg_info_util_ratio.png')
 plt.show()
 
-# 4. Replace the title to "Information Usage Over Time (seed=40)"
-# Correct time dimension
-info_usage_rl_seed40 = info_usage_rl[39, :]
-info_usage_vicsek_seed40 = info_usage_vicsek[39, :]
-time = np.arange(0, len(info_usage_rl_seed40) * 0.1, 0.1)
+# 4. Information Usage Over Time
+for seed in range(num_seeds):
+    info_util_ratio_at_seed_rl = info_util_ratio_rl[seed, :]
+    info_util_ratio_at_seed_vicsek = info_util_ratio_vicsek[seed, :]
+    time = np.arange(0, len(info_util_ratio_at_seed_rl) * 0.1, 0.1)
 
-plt.figure(figsize=(10, 6))
-plt.plot(time, info_usage_vicsek_seed40, label='Vicsek', color='tab:blue', linewidth=3, linestyle='--')
-plt.plot(time, info_usage_rl_seed40, label='RL', color='tab:green', linewidth=2)
-plt.title('Information Usage Over Time (seed=40)')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Information Usage')
-plt.legend()
-plt.show()
+    plt.figure(figsize=(10, 6))
+    plt.grid(True)
+    plt.plot(time, info_util_ratio_at_seed_vicsek, label='Vicsek', color='tab:blue', linewidth=3, linestyle='--')
+    plt.plot(time, info_util_ratio_at_seed_rl, label='RL', color='tab:green', linewidth=1.5)
+    plt.title(f'Information Usage Over Time (seed={seed})')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Information Utilization Ratio')
+    plt.xlim(0, max_time_steps * 0.1)
+    plt.ylim(0, 1.05)
+    plt.legend()
+    plt.savefig(f'../../data/240814/figs/240814_s{seed}_info_util_ratio.png')
+    # plt.show()
+    plt.close()
 
-# 5. Replace the title and y-label in the alignment plot
-plt.figure(figsize=(10, 6))
-plt.plot(time, alignment_hist_40_vicsek, label='Vicsek', color='tab:blue', linewidth=3, linestyle='--')
-plt.plot(time, alignment_hist_40_rl, label='RL', color='tab:green', linewidth=2)
-plt.title('Order Parameter Over Time (seed=40)')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Order Parameter')
-plt.legend()
-plt.show()
+# 5. Order Parameter Over Time
+for seed in range(num_seeds):
+    order_param_hist_at_seed_rl = order_parameter_rl[seed, :]
+    order_param_hist_at_seed_vicsek = order_parameter_vicsek[seed, :]
+    time = np.arange(0, len(order_param_hist_at_seed_rl) * 0.1, 0.1)
+
+    plt.figure(figsize=(10, 6))
+    plt.grid(True)
+    plt.plot(time, order_param_hist_at_seed_vicsek, label='Vicsek', color='tab:blue', linewidth=3, linestyle='--')
+    plt.plot(time, order_param_hist_at_seed_rl, label='RL', color='tab:green', linewidth=1.6)
+    plt.title(f'Order Parameter Over Time (seed={seed})')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Order Parameter')
+    plt.xlim(0, max_time_steps * 0.1)
+    plt.ylim(0, 1.04)
+    plt.legend()
+    plt.savefig(f'../../data/240814/figs/240814_s{seed}_order_parameter.png')
+    # plt.show()
+    plt.close()
+
+print("Done!")
